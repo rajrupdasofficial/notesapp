8b635d869d5ba76be24837ece6b07dfb90bd1413
RajrupDasid <rajrupdasid@outlook.com>
2022-01-14 18:52:57 +0530

    Important componants made and Notes list added with fake data

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>My Header</h1>
+    </div>
+  );
+};
+export default Header;
---
+++ src/pages/NoteListPage.js
@@ -0,0 +1,14 @@
+import react from "react";
+import notes from "../assets/data";
+const NotesListPage = () => {
+  return (
+    <div>
+      <div className="notes-list">
+        {notes.map((note) => (
+          <p>{note.body}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default NotesListPage;
